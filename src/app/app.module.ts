ee5f7adf018090b44cdefb9bdd85f479bc87051d
Oumar Diallo <oumar94diallo@gmail.com>
2021-06-22 17:36:00 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProgrammeComponent } from './programme/programme.component';
+import { ThematiqueComponent } from './thematique/thematique.component';
+import { QrcodeComponent } from './qrcode/qrcode.component';
+import { NotificationComponent } from './notification/notification.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgrammeComponent,
+    ThematiqueComponent,
+    QrcodeComponent,
+    NotificationComponent,
+    ConnexionComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,
